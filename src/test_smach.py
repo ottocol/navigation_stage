0c59c11cc4eb278e0c279bf7c92aefb6ec3ece55
ottocol <otto@dccia.ua.es>
2023-11-15 02:41:04 +0100

    mejorado ejemplo

--- src/test_smach.py
+++ src/test_smach.py
@@ -16,19 +16,25 @@ class Uno(State):
 
 class Dos(State):
     def __init__(self):
-        State.__init__(self, outcomes=['success'])
+        State.__init__(self, outcomes=['success','end'])
+        self.veces = 0
     
     def execute(self, userdata):
         print('dos')
         sleep(1)
-        return 'success'
+        self.veces+=1
+        print("veces estado DOS", self.veces)
+        if (self.veces>=3):
+            return 'end'
+        else:
+            return 'success'
 
 if __name__ == '__main__':
     rospy.init_node("test_smach")
-    sm = StateMachine(outcomes=['success'])
+    sm = StateMachine(outcomes=['stop'])
     with sm:
         StateMachine.add('UNO', Uno(), transitions={'success':'DOS'})
-        StateMachine.add('DOS', Dos(), transitions={'success':'UNO'})
+        StateMachine.add('DOS', Dos(), transitions={'success':'UNO', 'end':'stop'})
     sis = smach_ros.IntrospectionServer('server_name', sm, '/SM_ROOT')
     sis.start()
     sm.execute()
